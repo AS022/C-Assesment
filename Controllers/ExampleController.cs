cd2763438ed8cdf87861303c8fcfc36f106fc669
Amrita Singh <69029326+AS022@users.noreply.github.com>
2020-10-03 16:03:50 +0530

    Add files via upload
    CustomerMVC

---
+++ Controllers/ExampleController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoustmerApp.Models;
+
+namespace CoustmerApp.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ViewResult AllCoustomer()
+        {
+            var context = new AssesmentEntities();
+            var model = context.CustTables.ToList();
+            return View(model);
+        }
+        public ViewResult Find(string id)
+        {
+            int custID = int.Parse(id);
+            var context = new AssesmentEntities();
+            var model = context.CustTables.FirstOrDefault((e) => e.CustID == custID);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Find(CustTable cst)
+        {
+            var context = new AssesmentEntities();
+            var model = context.CustTables.FirstOrDefault((e) => e.CustID == cst.CustID);
+            model.CustName = cst.CustName;
+            model.CustAddress = cst.CustAddress;
+            context.SaveChanges();
+            return RedirectToAction(" AllCoustomer");
+        }
+
+        public ViewResult NewCustomer()
+        {
+            var model = new CustTable();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult NewCustomer(CustTable cst)
+        {
+            var context = new AssesmentEntities();
+            context.CustTables.Add(cst);
+            context.SaveChanges();
+            return RedirectToAction("AllCoustomer");
+        }
+
+
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoustmerApp.Models
+{
+    public class Customer
+    {
+        public class MyData
+        {
+            public string CustID { get; set; }
+            public string CustName { get; set; }
+            public string CustAddress { get; set; }
+
+            public override string ToString()
+            {
+
+                return $"The EmployeeID:{CustID}<p>The name:{CustName}<p>The Employee Address:{CustAddress}<p><p>";
+            }
+        }
+    }
+}
